f2deed66b552102658b5977bcc223052fe10eac8
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-04-21 23:09:42 +0530

    p2

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
 
