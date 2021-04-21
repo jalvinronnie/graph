919ae0a918d667e7af472a80536aafe29279e529
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-04-21 22:50:01 +0530

    test

--- server/index.js
+++ server/index.js
@@ -44,10 +44,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname,"client", "build", "index.html"));
+  // });
 }
+
+app.get('/', (req,res) => res.send('Hello???'))
 
 const port = process.env.PORT || 5000
 
